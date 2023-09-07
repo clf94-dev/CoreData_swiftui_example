718e9258f028fa5806f3cd425d039076951552d9
clf94-dev <clf94@hotmail.es>
2023-09-07 10:28:24 +0200

    - add new entity coreData

---
+++ Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  Core data demo swiftui
+//
+//  Created by Carmen Lucas on 7/9/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
